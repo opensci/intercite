13045860a621b3f200166b792378ff7f17f6f787
Mike Chelen <michael.chelen@gmail.com>
2014-10-29 00:18:20 -0400

    promises?

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 
 function doi2pmid(doi) {
 
+  var url = "http://www.corsproxy.com/www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=my_tool&email=my_email@example.com&ids="+doi+"&format=json";
 
-return pmid;
+  var foo = $.getJSON( url, function( data ) {
+    for (i=0;i<data.records.length;i++) {
+      root_pmid = data.records[i].pmid;
+      return root_pmid;
+    }
+  })
+    .fail(function(error) {
+      console.log("Failed!", error);
+    });
+
+  
 }
-
-
-
-
-
 
 doi = "10.1371/journal.ppat.1003211";
 
-doi2pmid_url = "http://www.corsproxy.com/www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=my_tool&email=my_email@example.com&ids="+doi+"&format=json";
+foo = doi2pmid(doi);
 
+console.log(foo);
+ 
 
-var foo = $.getJSON( doi2pmid_url, function( data ) {
-  for (i=0;i<data.records.length;i++) {
-    root_pmid = data.records[i].pmid;
-  }
-})
-  .fail(function() {
-    console.log( "error" );
-  });
-  
-
-foo.done(function() {
-  console.log( "bar: "+bar );
-  
-  reflist_url = "http://www.corsproxy.com/www.ebi.ac.uk/europepmc/webservices/rest/MED/"+root_pmid+"/references/1/json";
+  /*
+reflist_url = "http://www.corsproxy.com/www.ebi.ac.uk/europepmc/webservices/rest/MED/"+root_pmid+"/references/1/json";
   
   var baz = $.getJSON( url_2, function( data ) {
   for (i=0;i<data.referenceList.reference.length;i++) {
@@ -39,8 +35,6 @@ foo.done(function() {
     console.log( "error" );
   });
 
-  
-  
-})  
-  
+ */
+
   
