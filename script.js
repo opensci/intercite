a7891ba79c1d51425d2dc5fd27ca22b4854b1f75
Mike Chelen <michael.chelen@gmail.com>
2014-10-28 23:34:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+function doi2pmid(doi) {
+
+
+return pmid;
+}
+
+
+
+
+
+
+doi = "10.1371/journal.ppat.1003211";
+
+doi2pmid_url = "http://www.corsproxy.com/www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=my_tool&email=my_email@example.com&ids="+doi+"&format=json";
+
+
+var foo = $.getJSON( doi2pmid_url, function( data ) {
+  for (i=0;i<data.records.length;i++) {
+    root_pmid = data.records[i].pmid;
+  }
+})
+  .fail(function() {
+    console.log( "error" );
+  });
+  
+
+foo.done(function() {
+  console.log( "bar: "+bar );
+  
+  reflist_url = "http://www.corsproxy.com/www.ebi.ac.uk/europepmc/webservices/rest/MED/"+root_pmid+"/references/1/json";
+  
+  var baz = $.getJSON( url_2, function( data ) {
+  for (i=0;i<data.referenceList.reference.length;i++) {
+    console.log(data.referenceList.reference[i].id);
+  }
+})
+  .fail(function() {
+    console.log( "error" );
+  });
+
+  
+  
+})  
+  
+  
