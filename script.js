92243f31299b41cd59e1715482ccdf3fb3b6c755
Mike Chelen <michael.chelen@gmail.com>
2014-10-29 00:20:26 -0400

    sort of working

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-
-function doi2pmid(doi) {
-
-
-return pmid;
-}
-
-
-
-
-
-
 doi = "10.1371/journal.ppat.1003211";
 
 doi2pmid_url = "http://www.corsproxy.com/www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool=my_tool&email=my_email@example.com&ids="+doi+"&format=json";
@@ -26,11 +14,11 @@ var foo = $.getJSON( doi2pmid_url, function( data ) {
   
 
 foo.done(function() {
-  console.log( "bar: "+bar );
+  console.log( "root_pmid: "+root_pmid );
   
   reflist_url = "http://www.corsproxy.com/www.ebi.ac.uk/europepmc/webservices/rest/MED/"+root_pmid+"/references/1/json";
   
-  var baz = $.getJSON( url_2, function( data ) {
+  var baz = $.getJSON( reflist_url, function( data ) {
   for (i=0;i<data.referenceList.reference.length;i++) {
     console.log(data.referenceList.reference[i].id);
   }
